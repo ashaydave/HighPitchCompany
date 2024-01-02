92f81e56f2ded1b5ccd64bc9fa5ff160ed709c57
ashaydave <ashaydave@gmail.com>
2024-01-02 02:27:58 -0500

    My work keeps the company happy

---
+++ HighPitchCompany/HighPitchCompany.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+using HighPitchCompany.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighPitchCompany
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class HighPitchCompany : BaseUnityPlugin
+    {
+        private const string modGUID = "Ashay.HighPitchCompany";
+        private const string modName = "High Pitch Company";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static HighPitchCompany Instance;
+
+        internal ManualLogSource manualLogSource;
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            manualLogSource = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            manualLogSource.LogInfo("High Pitch Company Loaded");
+            harmony.PatchAll(typeof(HighPitchCompany));
+            harmony.PatchAll(typeof(SoundManagerPatch));
+        }
+    }
+}
---
+++ HighPitchCompany/Patches/SoundManagerPatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using GameNetcodeStuff;
+
+namespace HighPitchCompany.Patches
+{
+    [HarmonyPatch(typeof(SoundManager))]
+    internal class SoundManagerPatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        static void highPitchPatch(ref float[] ___playerVoicePitchTargets, ref float[] ___playerVoicePitches)
+        {
+            ___playerVoicePitches = new float[4] { 3f, 3f, 3f, 3f };
+            ___playerVoicePitchTargets = new float[4] { 3f, 3f, 3f, 3f };
+        }
+    }
+}
