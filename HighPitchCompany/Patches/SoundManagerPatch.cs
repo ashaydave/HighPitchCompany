f4c159b9a80899e22d3a78ed011e59ccba50670a
ashaydave <ashaydave@gmail.com>
2024-01-06 17:34:40 -0500

    Attempt 2 may keep the company happy

--- HighPitchCompany/Patches/SoundManagerPatch.cs
+++ HighPitchCompany/Patches/SoundManagerPatch.cs
@@ -8,15 +8,41 @@ using GameNetcodeStuff;
 
 namespace HighPitchCompany.Patches
 {
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+
+        static void playerControllerBPatch(PlayerControllerB playerControllerB)
+        {
+            SoundManager.Instance.playerVoicePitchTargets[playerControllerB.playerClientId] = 3f;
+        }
+    }
+
     [HarmonyPatch(typeof(SoundManager))]
     internal class SoundManagerPatch
     {
         [HarmonyPatch("Start")]
         [HarmonyPostfix]
-        static void highPitchPatch(ref float[] ___playerVoicePitchTargets, ref float[] ___playerVoicePitches)
+        static void highPitchPatch(PlayerControllerB playerControllerB, ref float[] ___playerVoicePitchTargets, ref float[] ___playerVoicePitches)
         {
             ___playerVoicePitches = new float[4] { 3f, 3f, 3f, 3f };
             ___playerVoicePitchTargets = new float[4] { 3f, 3f, 3f, 3f };
+
         }
     }
+
+    [HarmonyPatch(typeof(StartOfRound))]
+    internal class StartOfRoundPatch
+    {
+        [HarmonyPatch("UpdatePlayerVoiceEffects")]
+        [HarmonyPostfix]
+        static void updatePlayerVoiceEffectsPatch(PlayerControllerB playerControllerB)
+        {
+            SoundManager.Instance.playerVoicePitchTargets[playerControllerB.playerClientId] = 3f;
+            SoundManager.Instance.SetPlayerPitch(3f, (int)playerControllerB.playerClientId);
+        }
+    }
+
 }
